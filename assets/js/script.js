05b7fa1b8059039738fc074f807194cd0c00f375
asama <asamahayder@yahoo.dk>
2021-04-30 12:20:45 +0200

    testing screen jump on chat message problem fix 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -220,7 +220,7 @@ $(function(){
 		var item = document.createElement('li');
 		item.textContent = msg;
 		messages.appendChild(item);
-		window.scrollTo(0, document.body.scrollHeight);
+		//window.scrollTo(0, document.body.scrollHeight);
 	}
 
 	function updatePlayerCount(playerCount, maxPlayers){
