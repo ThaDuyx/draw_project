86c29ba60ff6c8c04ce759022e29d07fa5182ce3
asama <asamahayder@yahoo.dk>
2021-04-30 12:19:08 +0200

    testing screen jump on chat message problem fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,12 +160,7 @@ $(function(){
 	});
 
 	socket.on('chat message', function(msg) {
-		var item = document.createElement('li');
-
-		item.textContent = msg;
-
-		messages.appendChild(item);
-		window.scrollTo(0, document.body.scrollHeight);
+		createChatMessage(msg);
 	});
 
 	socket.on('timer', function (counter) {
